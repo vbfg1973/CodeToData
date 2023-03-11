12f818eb44f01dfaa40ffa1a79b3533b83e5c12f
Chris Russell <cgrussell@gmail.com>
2023-03-11 07:55:50 +0000

    Updated docs and helptexts

--- src/CodeToData.Domain/Verbs/Definitions/DefinitionsOptions.cs
+++ src/CodeToData.Domain/Verbs/Definitions/DefinitionsOptions.cs
@@ -2,7 +2,7 @@
 
 namespace CodeToData.Domain.Verbs.Definitions
 {
-    [Verb("definitions", HelpText = "")]
+    [Verb("definitions", HelpText = "Find all class definitions and list their source")]
     public sealed class DefinitionsOptions : BaseSolutionOptions
     {
     }
--- src/CodeToData.Domain/Verbs/Repetition/RepetitionOptions.cs
+++ src/CodeToData.Domain/Verbs/Repetition/RepetitionOptions.cs
@@ -3,7 +3,7 @@ using CommandLine;
 
 namespace CodeToData.Domain.Verbs.Repetition
 {
-    [Verb("repetition")]
+    [Verb("repetition", HelpText = "Walk all ASTs in the solution and detect repeating patterns in the code")]
     public class RepetitionOptions : BaseSolutionOptions
     {
     }
